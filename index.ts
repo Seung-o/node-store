d6b26fc488cc249936e60ff285da1d3055e6940b
seungo <ilkhso@gmail.com>
2022-02-24 23:01:58 +0900

    Router 설정

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-// import express from "express";
-const express = require("express");
+import express from "express";
+import goodsRouter from "./routes/goods";
+import userRouter from "./routes/user";
+
 const app = express();
 const port = 3000;
 
+app.use("/goods", goodsRouter);
+app.use("/user", userRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hi. This is express router");
 });
 
 app.listen(port, () => {
