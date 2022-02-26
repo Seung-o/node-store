db0051136b41174c577087b6bd821d16971a623d
seungo <ilkhso@gmail.com>
2022-02-26 17:31:41 +0900

    create home page and detail page

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,26 @@ const goods_1 = __importDefault(require("./routes/goods"));
 const user_1 = __importDefault(require("./routes/user"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+app.use(express_1.default.static("public"));
 app.use("/goods", goods_1.default);
 app.use("/user", user_1.default);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.send("Hi. This is express router");
+    res.send("Hello it is node store");
+});
+app.get("/test", (req, res) => {
+    const name = req.query.name;
+    res.render("test", { name });
+});
+app.get("/home", (req, res) => {
+    res.render("index");
+});
+app.get("/detail", (req, res) => {
+    const goodsId = req.query.goodsId;
+    res.render("detail", { goodsId });
 });
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -5,11 +5,29 @@ import userRouter from "./routes/user";
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
 app.use("/goods", goodsRouter);
 app.use("/user", userRouter);
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hi. This is express router");
+  res.send("Hello it is node store");
+});
+app.get("/test", (req, res) => {
+  const name = req.query.name;
+  res.render("test", { name });
+});
+app.get("/home", (req, res) => {
+  res.render("index");
+});
+
+app.get("/detail", (req, res) => {
+  const goodsId = req.query.goodsId;
+  res.render("detail", { goodsId });
 });
 
 app.listen(port, () => {
